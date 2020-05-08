aa494f27e223b4dc067d968ada47a7bed4cad3e0
Jesseliz <choque101108@gmail.com>
2020-05-08 13:03:48 -0500

    second news

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:club_konecta/src/pages/events_page.dart';
 import 'package:club_konecta/src/pages/profile_page.dart';
+import 'package:club_konecta/src/pages/second_news.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:club_konecta/src/pages/teky.dart';
 import 'kuentanos.dart';
@@ -19,7 +20,7 @@ class _HomePageState extends State<HomePage> {
     KuentanosPage(),
     DisplaysWidget(Colors.green),
     EventPage(),
-    DisplaysWidget(Colors.pink),
+    SecondNewsBenefits(),
   ];
 
   @override
--- lib/src/pages/second_news.dart
+++ lib/src/pages/second_news.dart
@@ -1,87 +1,251 @@
 import 'package:club_konecta/src/providers/news_provider.dart';
+import 'rating.dart';
+import 'package:club_konecta/src/model/news_json_model.dart';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SecondNews extends StatelessWidget {
-  // const SecondNews({Key key}) : super(key: key);}
+class SecondNewsBenefits extends StatelessWidget {
   final HttpServiceNews httpService = HttpServiceNews();
-
   @override
   Widget build(BuildContext context) {
-     return
-      Scaffold(
-        appBar: AppBar(
-        title: Text("Noticias"),
-        backgroundColor: Color(0xff03DAC5),
+    return SingleChildScrollView(
+      child: Container(
+        padding: new EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+        child: Column(
+//        crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Align(
+              heightFactor: 2.5,
+              alignment: Alignment.centerLeft,
+              child: Container(
+                margin: EdgeInsets.only(bottom: 05.0),
+                child: Text(
+                  "Entérate",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              padding: new EdgeInsets.all( 10.0),
+              child: FutureBuilder(
+                future: httpService.getNews(),
+                builder:
+                    (BuildContext context, AsyncSnapshot<List<Noticia>> snapshot) {
+                  if (snapshot.hasData) {
+                    List<Noticia> posts = snapshot.data;
+
+                    return Column(
+                      children: posts
+                          .map(
+                            (Noticia post) => Card(
+                              child: Column(
+                                children: <Widget>[
+                                  Container(
+                                    child: Container(
+//                  margin: EdgeInsets.all(5.0),
+                                      child: ClipRRect(
+                                          borderRadius: BorderRadius.vertical(
+                                              top: Radius.circular(5.0)),
+                                          child: Stack(
+                                            children: <Widget>[
+                                              Image.asset('assets/example1.png',
+                                                  fit: BoxFit.cover, width: 1000.0),
+                                              Positioned(
+                                                bottom: 0.0,
+                                                left: 0.0,
+                                                right: 0.0,
+                                                child: Container(
+                                                  decoration: BoxDecoration(
+                                                    gradient: LinearGradient(
+                                                      colors: [
+                                                        Color.fromARGB(
+                                                            200, 0, 0, 0),
+                                                        Color.fromARGB(0, 0, 0, 0)
+                                                      ],
+                                                      begin: Alignment.bottomCenter,
+                                                      end: Alignment.topCenter,
+                                                    ),
+                                                  ),
+                                                  padding: EdgeInsets.symmetric(
+                                                      vertical: 10.0,
+                                                      horizontal: 20.0),
+                                                  child: Text(
+                                                    post.titulo,
+                                                    style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontSize: 20.0,
+                                                      fontWeight: FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          )),
+                                    ),
+                                  ),
+                                  Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Container(
+                                      margin:
+                                          EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.stretch,
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        children: <Widget>[
+                                          Padding(
+                                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                                            child: Text(
+                                              post.fechaPublicacion,
+                                              style: TextStyle(
+                                                fontSize: 16.0,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ),
+                                          Text(
+                                            post.resumen,
+                                            style: TextStyle(
+                                              fontSize: 16.0,
+                                            ),
+                                          ),
+                                          Container(
+                                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                                            child: Rating(),),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    );
+                  }
+                  return CircularProgressIndicator();
+                },
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Align(
+              heightFactor: 2.5,
+              alignment: Alignment.centerLeft,
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 15.0),
+                child: Text(
+                  "Beneficios",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            FutureBuilder(
+              future: httpService.getNews(),
+              builder:
+                  (BuildContext context, AsyncSnapshot<List<Noticia>> snapshot) {
+                if (snapshot.hasData) {
+                  List<Noticia> posts = snapshot.data;
+
+                  return Column(
+                    children: posts
+                        .map(
+                          (Noticia post) => Card(
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  child: Container(
+//                  margin: EdgeInsets.all(5.0),
+                                    child: ClipRRect(
+                                        borderRadius: BorderRadius.vertical(
+                                            top: Radius.circular(5.0)),
+                                        child: Stack(
+                                          children: <Widget>[
+                                            Image.asset('assets/example1.png',
+                                                fit: BoxFit.cover, width: 1000.0),
+                                            Positioned(
+                                              bottom: 0.0,
+                                              left: 0.0,
+                                              right: 0.0,
+                                              child: Container(
+                                                decoration: BoxDecoration(
+                                                  gradient: LinearGradient(
+                                                    colors: [
+                                                      Color.fromARGB(
+                                                          200, 0, 0, 0),
+                                                      Color.fromARGB(0, 0, 0, 0)
+                                                    ],
+                                                    begin: Alignment.bottomCenter,
+                                                    end: Alignment.topCenter,
+                                                  ),
+                                                ),
+                                                padding: EdgeInsets.symmetric(
+                                                    vertical: 10.0,
+                                                    horizontal: 20.0),
+                                                child: Text(
+                                                  post.titulo,
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 20.0,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        )),
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Container(
+                                    margin:
+                                        EdgeInsets.symmetric(horizontal: 20.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.stretch,
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      children: <Widget>[
+                                        Text(
+                                          post.titulo,
+                                          style: TextStyle(
+                                            fontSize: 16.0,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          post.resumen,
+                                          style: TextStyle(
+                                            fontSize: 16.0,
+                                          ),
+                                        ),
+                                        Rating()
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                        .toList(),
+                  );
+                }
+                return CircularProgressIndicator();
+              },
+            ),
+          ],
         ),
-        
-        body:new Container (
-      //    padding: new EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-      //   child: new Column(
-      //     // mainAxisAlignment: MainAxisAlignment.spaceAround,
-      //     children: <Widget>[
-      //       ListTile(
-              
-      //         title: Text(
-      //           'Entérate',
-      //           style: TextStyle(
-      //             fontFamily: 'Monserrate',
-      //             fontSize: 20,
-      //             fontWeight: FontWeight.bold,
-      //           ),
-      //         ),
-      //       ),
-      //   Expanded(
-      //   child: FutureBuilder(
-      //   future: httpService.getEvents(),
-      //   builder: (BuildContext context, AsyncSnapshot<List<Event>> snapshot) {
-      //     if (snapshot.hasData) {
-      //       List<Event> event = snapshot.data;
-      //       return ListView(
-      //         children: event
-      //             .map((Event event) =>
-      //             Card(
-      //               child: new Column(
-      //                 children: <Widget>[
-      //                   Container(
-      //                     padding: EdgeInsets.symmetric(vertical:20.0, horizontal: 10.0),
-      //                     child: Text(event.titulo, style: TextStyle(
-      //                       fontFamily: 'Monserrate',
-      //                       fontSize: 18,
-      //                     ),),
-      //                     ),
-      //                     FadeInImage(
-      //                       placeholder: AssetImage('assets/original.gif'), 
-      //                       image: NetworkImage(event.banner),
-      //                       fadeInDuration: Duration(milliseconds: 200),
-      //                       height: 300.0,
-      //                       fit: BoxFit.cover,
-      //                       ),
-                           
-      //                     Container(
-      //                     padding: EdgeInsets.symmetric(vertical:20.0, horizontal: 10.0),
-      //                     child: Text(event.contenido, style: TextStyle(
-      //                       fontFamily: 'Monserrate',
-      //                       fontSize: 17,
-      //                     ),
-      //                     textAlign: TextAlign.justify,
-      //                     ),
-      //                     ),
-      //                     ],),),
-      //                 )
-      //             .toList(),
-      //       );
-           
-      //     }
-      //     return Center(child: CircularProgressIndicator(
-      //       backgroundColor: Color(0xff03DAC5),
-      //     ),);
-      //   },
-      // ),
-      // ),
-      // ],
-      // ),
-    ),
-      
+      ),
     );
+
   }
 }
